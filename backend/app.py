a077e694a6f6f42ce95043cc1c95085f4c4f5a56
Rohan-V <rohankvij@gmail.com>
2022-08-20 20:57:50 -0700

    Finished sprint 1 post routes

--- backend/app.py
+++ backend/app.py
@@ -40,7 +40,7 @@ client = pymongo.MongoClient(
 
 db = client["sportal"]
 users = db["users"]
-
+posts = db["posts"]
 
 # Routes
 
@@ -57,17 +57,18 @@ def running_message():
 def login():
     """
     Login a user.
+
     :return: A JWT access token if the user is found, an error otherwise.
     """
-    username = request.json.get("username", None)
+    email = request.json.get("email", None)
     password = request.json.get("password", None)
 
-    user = query_user(username)
+    user = query_user(email)
 
     if not user:
         return jsonify({"message": "User not found"}), 404
     if password != user["password"]:
-        return jsonify({"msg": "Incorrect or password"}), 401
+        return jsonify({"msg": "Incorrect password"}), 401
 
     access_token = create_access_token(identity=str(user["_id"]))
     refresh_token = create_refresh_token(identity=str(user["_id"]))
@@ -78,18 +79,32 @@ def login():
 def signup():
     """
     Signup a user.
+
     :return: A JWT access token if the user is created, an error otherwise.
     """
-    username = request.json.get("username", None)
+    name = request.json.get("name", None)
     password = request.json.get("password", None)
+    email = request.json.get("email", None)
+    phone = request.json.get("phone", None)
+    gender = request.json.get("gender", None)
+    dob = request.json.get("dob", None)
 
-    user = query_user(username)
+    user = query_user(email)
 
     if user:
-        return jsonify({"message": "User already exists"}), 400
+        return jsonify({"message": "User with that email already exists"}), 400
+
+    signup_document = {
+        "name": name,
+        "password": password,
+        "email": email,
+        "phone": phone,
+        "gender": gender,
+        "dob": dob
+    }
 
     _id = users.insert_one(
-        {"username": username, "password": password}).inserted_id
+        signup_document).inserted_id
 
     access_token = create_access_token(identity=str(_id))
     refresh_token = create_refresh_token(identity=str(user["_id"]))
@@ -102,23 +117,161 @@ def signup():
 def refresh():
     """
     Refresh a JWT access token.
+
     :return: A JWT access token if the user is found and the token is valid, an error otherwise.
     """
     identity = get_jwt_identity()
     access_token = create_access_token(identity=identity)
     return jsonify(access_token=access_token)
 
+## -- End User Management -- ##
+
+## -- Post Management -- ##
+
+
+@app.route("/posts/create", methods=["POST"])
+@jwt_required()
+def create_post():
+    """
+    Create a post.
+
+    :return: A message indicating the post was created and its ID.
+    """
+
+    sport = request.json.get("sport", None)
+    location = request.json.get("location", None)
+    poster = request.json.get("poster", None)
+    start_date = request.json.get("date", None)
+    end_date = request.json.get("date", None)
+    description = request.json.get("description", None)
+    level = request.json.get("level", None)
+    max_players = request.json.get("max_players", None)
+    players = [poster]
+    is_open = True
+
+    post_document = {
+        "sport": sport,
+        "location": location,
+        "poster": poster,
+        "start_date": start_date,
+        "end_date": end_date,
+        "description": description,
+        "level": level,
+        "max_players": max_players,
+        "players": players,
+        "is_open": is_open
+    }
+
+
+    _id = posts.insert_one(
+        post_document).inserted_id
+
+    return jsonify({"message": "Success", "id": str(_id)}), 200
+
+@app.route("/posts/<post_id>", methods=["GET"])
+def get_post(post_id):
+    """
+    Get a post with the given ID.
+
+    :param post_id: The ID of the post to get.
+    :return: The post if found, an error otherwise.
+    """
+
+    post = posts.find_one({"_id": ObjectId(post_id)})
+    if not post:
+        return jsonify({"message": "Post not found"}), 404
+
+    return jsonify(post), 200
+
+@app.route("/posts/<post_id>/add/<email>", methods=["PATCH"])
+@jwt_required()
+def add_participant(post_id, email):
+    """
+    Add a user to the list of participants of a post.
+
+    :param post_id: The ID of the post to update.
+    :param email: The email of the user to add to the participants list.
+    :return: A message indicating the post was updated.
+    """
+
+    post = posts.find({"_id": ObjectId(post_id)})
+    if not post:
+        return jsonify({"message": "Post not found"}), 404
+    elif post["is_open"]:
+        return jsonify({"message": "Post is already closed"}), 400
+
+    posts.update_one({"_id": ObjectId(post_id)}, {'$push': {'players': email}})
+
+    return jsonify({"message": "Success"}), 200
+
+@app.route("/posts/<post_id>/remove/<email>", methods=["PATCH"])
+@jwt_required()
+def remove_participant(post_id, email):
+    """
+    Remove a user from the list of participants of a post.
+
+    :param post_id: The ID of the post to update.
+    :param email: The email of the user to remove from the participants list.
+    :return: A message indicating the post was updated.
+    """
+
+    post = posts.find({"_id": ObjectId(post_id)})
+    if not post:
+        return jsonify({"message": "Post not found"}), 404
+    elif users.find({"_id": ObjectId(get_jwt_identity())})["email"] != post["email"]:
+        return jsonify({"message": "You are not the owner of this post"}), 400
+    elif post["is_open"]:
+        return jsonify({"message": "Post is already closed"}), 400
+
+    posts.update_one({"_id": ObjectId(post_id)}, {'$pull': {'players': email}})
+
+    return jsonify({"message": "Success"}), 200
+
+@app.route("/posts/<post_id>/close", methods=["PATCH"])
+@jwt_required()
+def close_post(post_id):
+    """
+    Close a post.
+
+    :param post_id: The ID of the post to update.
+    :return: A message indicating the post was updated.
+    """
+
+    post = posts.find({"_id": ObjectId(post_id)})
+
+    if not post:
+        return jsonify({"message": "Post not found"}), 404
+
+    if users.find({"_id": ObjectId(get_jwt_identity())})["email"] != post["email"]:
+        return jsonify({"message": "You are not the owner of this post"}), 400
+
+    posts.update_one({"_id": ObjectId(post_id)}, {'$set': {'is_open': False}})
+
+    return jsonify({"message": "Success"}), 200
+
+@app.route("/posts/all", methods=["GET"])
+@jwt_required()
+def get_all_posts():
+    """
+    Get all posts.
+
+    :return: All posts.
+    """
+
+    all_posts = list(posts.find())
+    return jsonify(all_posts), 200
+
 
 # Helper functions
 
 
-def query_user(username: str):
+def query_user(email: str, field="email"):
     """
-    Query the database for a user with the given username.
+    Query the database for a user with the given email.
     :param username: The username to search for.
     :return: The user if found, None otherwise.
     """
-    user = users.find_one({"username": username})
+    user = users.find_one({field: email})
     print(user)
     return None if not user else user
 
